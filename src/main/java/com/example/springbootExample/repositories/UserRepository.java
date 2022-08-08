1cfefa70a0c442e57052e22b6fd3ee9e952cd8ba
Steffi Prabha <steffi.prabha@gmail.com>
2022-08-08 23:15:31 +0530

    swagger removed will implement later

--- src/main/java/com/example/springbootExample/repositories/UserRepository.java
+++ src/main/java/com/example/springbootExample/repositories/UserRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.springbootExample.models.User;
@@ -16,16 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Override
     @Query
     List<User> findAll();
-
  
     @Deprecated
     User findFirstBySso(Long sso);
-
-    //List<User> findByRoleId(Long roleId);
-
-   /*  @Query("select u from User u join u.role r where r.description = :roleName" , nativeQuery=true)
-    List<User> findUserByRoleName(@Param("roleName") String roleName); */
-
-    //List<User> findByPhoneNumberNotNullAndCarrierNotNullOrderByName();
 
 }
